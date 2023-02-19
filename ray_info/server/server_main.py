fb14456dd8dfcd235c44ab6b8f8910c0cfc1529e
maxiee <maxieewong@gmail.com>
2023-02-20 00:15:16 +0800

    info-list api

--- ray_info/server/server_main.py
+++ ray_info/server/server_main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
 import uvicorn
 
+from ray_info.db import Info
+
+
 def server_main():
     app = FastAPI()
 
-    @app.get('/')
+    @app.get("/")
     async def hello():
         return "Hello, world!"
-    
+
+    @app.get("/info/list")
+    async def info_list(page: int = 1, per_page: int = 20):
+        query = Info.select().order_by(Info.updated.desc())
+        total = query.count()
+        info_list = query.paginate(page, per_page)
+        return {
+            "total": total,
+            "info_list": [info.__dict__["__data__"] for info in info_list],
+        }
+
     uvicorn.run(app, host="0.0.0.0", port=1127)
